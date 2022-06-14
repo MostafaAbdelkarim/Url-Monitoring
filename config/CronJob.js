f3a29d2bcf3d722148d7c962dc90f5bc6f1bdd8e
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2022-06-14 17:24:56 +0200

    reports updating successfully to db

--- config/CronJob.js
+++ config/CronJob.js
@@ -2,12 +2,12 @@ const nodecron = require('node-cron');
 const Report = require('../models/report');
 const ReportSerivce = require('../services/report');
 
-//scheduling updating reports status every 5 seconds 
+//scheduling updating reports status every second 
 const updateReport = nodecron.schedule('* * * * * *', async function() {
     
     try {
         await ReportSerivce.scheduledUpdateForReports();
-        // console.log('Updating reports each 5 seconds');
+        // console.log('Updating reports each 1 seconds');
     }
     catch(error){
         console.log(error)
--- services/report.js
+++ services/report.js
@@ -43,23 +43,16 @@ const getReportsByTag = async (req, res) => {
 //this implementation is not correct as i did not reach as to how get this detailed report accurately
 const scheduledUpdateForReports = async (req, res) => {
     try{
-        //Logic needed to be implemented should be as follows:
-        //selecting all checks and only getting their checkId and url
-        //using axios to send a get request for each url and get reponse
-        //getting each report that is based in checkId and updating the report based on axios reponse
-        //after updating each report with approperiate data we save them to db
-        //this method will be called every x time interval(1 hour, every 12hrs .. etc) using node-cron
         const checks = await Check.find().select('checkId url'); 
         if(!checks) return error = {message: 'Could not find any checks'};
-        
         for(let i = 0; i < checks.length; i++){
             const result = await axiosCheck.getStatusCode(checks[i].url);
             let report = await Report.findOne({checkId: checks[i]._id});
             if(!report) return error = {message: 'could not find report related to this check'};
             if(result.status === 200){
-                report = await report.findOneAndUpdate({checkId: checks[i]._id},{
+                report = await Report.findOneAndUpdate({checkId: checks[i]._id},{
                     status: 200,
-                    availability: (report.uptime / (report.uptime + report.downtime)) * 100,
+                    availability: ((report.uptime / (report.uptime + report.downtime)) * 100),
                     outages: report.outages,
                     downtime: report.outages,
                     uptime: report.uptime + 1, 
@@ -68,9 +61,9 @@ const scheduledUpdateForReports = async (req, res) => {
                 });
             }
             else{
-                report = await report.findOneAndUpdate({checkId: checks[i]._id}, {
+                report = await Report.findOneAndUpdate({checkId: checks[i]._id}, {
                     status: result.status,
-                    availability: (report.uptime / (report.uptime + report.downtime)) * 100,
+                    availability: ((report.uptime / (report.uptime + report.downtime)) * 100),
                     outages: report.outages + 1,
                     downtime: report.downtime + 1,
                     uptime: report.uptime - 1, 
@@ -79,6 +72,7 @@ const scheduledUpdateForReports = async (req, res) => {
                 });
             }
         }
+        console.log('Reports updated and saved!');
     }
     catch(error){
         console.log('scheduledUpdateForReports error: ' + error)
